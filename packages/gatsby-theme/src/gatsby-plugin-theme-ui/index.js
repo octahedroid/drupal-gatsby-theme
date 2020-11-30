39e589067c6f73a63f9397592e38f11bb4386df1
RosaIsela31 <rosa@octahedroid.com>
2020-11-30 12:44:11 -0600

    feat: add md, remove text object

--- packages/gatsby-theme/src/gatsby-plugin-theme-ui/index.js
+++ packages/gatsby-theme/src/gatsby-plugin-theme-ui/index.js
@@ -46,6 +46,7 @@ const bHeadings = {
 }
 const bShadows = {
   default: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+  md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
 }
 
 const bTransitions = {
@@ -154,17 +155,6 @@ export default {
       ":hover": {
         textDecoration: "underline",
       },
-    },
-  },
-  text: {
-    tiny: {
-      fontSize: "tiny",
-    },
-    lg: {
-      fontSize: "lg",
-    },
-    xl: {
-      fontSize: "xl",
     },
   },
   space: {
