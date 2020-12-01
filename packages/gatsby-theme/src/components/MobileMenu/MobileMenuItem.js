ba21d66a9286428d77382c18ca3ee82f878a4c8a
RosaIsela31 <rosa@octahedroid.com>
2020-12-01 11:43:52 -0600

    feat: added MobileMenu component

---
+++ packages/gatsby-theme/src/components/MobileMenu/MobileMenuItem.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Box } from "theme-ui"
+import PropTypes from "prop-types"
+import Link from "../Link"
+
+const MobileMenuItem = ({ children, to, sx }) => (
+  <Box as='li'>
+    <Link
+      to={to}
+      sx={{
+        textDecoration: "none",
+        color: "white",
+        "&:hover": {
+          textDecoration: "underline",
+        },
+        "&[aria-current=page]": {
+          textDecoration: "underline",
+        },
+        ...sx,
+      }}
+    >
+      {children}
+    </Link>
+  </Box>
+)
+
+MobileMenuItem.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+    .isRequired,
+  to: PropTypes.string.isRequired,
+  sx: PropTypes.shape({}),
+}
+
+MobileMenuItem.defaultProps = {
+  sx: null,
+}
+
+export default MobileMenuItem
---
+++ packages/gatsby-theme/src/components/MobileMenu/MobileMenuNavbar.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Box } from "theme-ui"
+import PropTypes from "prop-types"
+
+const MobileMenuNavbar = ({ children, ...props }) => (
+  <Box
+    as='ul'
+    __css={{
+      listStyleType: "none",
+      pl: 0,
+    }}
+    {...props}
+  >
+    {children}
+  </Box>
+)
+
+MobileMenuNavbar.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+    .isRequired,
+}
+
+export default MobileMenuNavbar
