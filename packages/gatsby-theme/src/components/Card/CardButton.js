cacd21c87b9a66f1e0cd2fd60e8cf3f292603c99
Carlos Dinarte <carlos@octahedroid.com>
2020-12-02 12:17:10 -0600

    Feat: add Card component

---
+++ packages/gatsby-theme/src/components/Card/CardButton.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types"
+import Button from "../Button"
+import Link from "../Link"
+
+const CardButton = ({ children, variant, to, ...props }) => (
+  <Button as={Link} variant={variant} to={to} {...props}>
+    {children}
+  </Button>
+)
+
+CardButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  variant: PropTypes.string,
+}
+
+CardButton.defaultProps = {
+  variant: "primary",
+}
+
+export default CardButton
