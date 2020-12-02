42d2d322156cf577ea484d92a1decd1771985cbb
edvicaty <edvicaty@hotmail.com>
2020-12-02 16:39:06 -0600

    feat: add Contact component and ContactBlock

---
+++ packages/gatsby-theme/src/blocks/ContactBlock/index.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types"
+import Contact from "../../components/Contact"
+
+const ContactBlock = ({ heading, align, contactInfo }) => {
+  return (
+    <Contact>
+      <Contact.Header align={align}>{heading}</Contact.Header>
+      <Contact.Grid align={align} contactInfo={contactInfo} />
+    </Contact>
+  )
+}
+
+ContactBlock.propTypes = {
+  heading: PropTypes.string.isRequired,
+  contactInfo: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      icon: PropTypes.string,
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  align: PropTypes.string,
+}
+
+ContactBlock.defaultProps = {
+  align: "center",
+}
+
+export default ContactBlock
---
+++ packages/gatsby-theme/src/components/Contact/index.js
@@ -0,0 +1,8 @@
+import Contact from "./Contact"
+import ContactGrid from "./ContactGrid"
+import ContactHeader from "./ContactHeader"
+
+Contact.Grid = ContactGrid
+Contact.Header = ContactHeader
+
+export default Contact
