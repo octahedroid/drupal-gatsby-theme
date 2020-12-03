6ea32e8c3aaecfcbf3f1acb1d81c4e081fcf0b2b
Edgar Villavicencio <edvicaty@hotmail.com>
2020-12-03 13:47:37 -0600

    feat: add Contact component and ContactBlock (#27)
    * feat: add Contact component and ContactBlock
    
    * add component imports to index.js
    
    * refactor: move argTypes to Meta tag

--- packages/gatsby-theme/src/components/index.js
+++ packages/gatsby-theme/src/components/index.js
@@ -1,3 +1,7 @@
 // Export your components here to make them availables via `import { componentName } from "ui"`
 export { default as Button } from "./Button"
 export { default as BlogHead } from "./BlogHead"
+export { default as Contact } from "./Contact"
+export { default as Heading } from "./Heading"
+export { default as Icon } from "./Icon"
+export { default as RichText } from "./RichText"
