e824ffa68b4fe0e5833d3f885809a061ef0aad87
Edgar Villavicencio <edvicaty@hotmail.com>
2020-12-03 11:34:44 -0600

    feat: add Footer component and FooterBlock block (#23)
    * feat: added footer component and footer block
    
    * feat: added footer component and footer block
    
    * fix: remove import React from react and add {forwardRef} from React

---
+++ packages/gatsby-theme/src/components/Footer/FooterBranding.js
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types"
+import { Box } from "theme-ui"
+import Link from "../Link"
+import Logo from "../Logo"
+
+const FooterBranding = ({ variant, width, to, ariaLabel }) => {
+  const fillOptions = {
+    dark: "#F7F7F7",
+    light: "#37375B",
+  }
+  return (
+    <Box
+      __css={{
+        px: ["xsmall", null, "medium"],
+        order: ["auto", 1],
+        mb: ["small", null, 0],
+        display: "flex",
+        justifyContent: ["center", null, "flex-start"],
+      }}
+    >
+      <Link to={to} ariaLabel={ariaLabel}>
+        <Logo fill={fillOptions[variant]} width={width} />
+      </Link>
+    </Box>
+  )
+}
+
+FooterBranding.propTypes = {
+  variant: PropTypes.string,
+  width: PropTypes.number,
+  to: PropTypes.string.isRequired,
+  ariaLabel: PropTypes.string.isRequired,
+}
+
+FooterBranding.defaultProps = {
+  variant: "dark",
+  width: 200,
+}
+
+export default FooterBranding
---
+++ packages/gatsby-theme/src/components/Footer/FooterMenuTitle.js
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types"
+import Heading from "../Heading"
+
+const FooterMenuTitle = ({ children }) => {
+  return (
+    <Heading as='h5' variant='accentUppercase'>
+      {children}
+    </Heading>
+  )
+}
+
+FooterMenuTitle.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default FooterMenuTitle
