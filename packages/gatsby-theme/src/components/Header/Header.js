a885a37a57367356f76c06efdf4a272691aab736
Melissa Castillo <65203904+melscastillo@users.noreply.github.com>
2020-12-03 11:33:38 -0600

    Feat GAT-27: add Header component and HeaderBlock (#22)
    * feat GAT-27: create Header Component
    
    * feat GAT-27: Create header and HeaderBlock component
    
    * added changes to header.js

---
+++ packages/gatsby-theme/src/components/Header/Header.js
@@ -0,0 +1,74 @@
+import { forwardRef } from "react"
+import PropTypes from "prop-types"
+import { Box } from "theme-ui"
+import { SkipNavLink } from "@reach/skip-nav"
+
+const SkipNavStyles = {
+  border: 0,
+  clip: "rect(0 0 0 0)",
+  height: 1,
+  width: 1,
+  margin: -1,
+  padding: 0,
+  overflow: "hidden",
+  position: "absolute",
+  ":focus": {
+    padding: "1rem",
+    position: "fixed",
+    top: 10,
+    left: 10,
+    background: "white",
+    zIndex: 100,
+    width: "auto",
+    height: "auto",
+    clip: "auto",
+  },
+}
+
+const Header = forwardRef(({ children, shadow }, ref) => {
+  return (
+    <Box
+      as='nav'
+      ref={ref}
+      __css={{
+        position: "fixed",
+        width: "100%",
+        zIndex: 30,
+        height: ["auto", null, 80],
+        top: 0,
+        left: 0,
+        color: "black",
+        backgroundColor: "white",
+        transition: "shadow",
+        boxShadow: shadow ? "default" : "",
+      }}
+    >
+      <Box as={SkipNavLink} __css={SkipNavStyles} />
+      <Box
+        __css={{
+          display: "flex",
+          flexWrap: "wrap",
+          p: [1, null, 0],
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+        variant='wrapper'
+        __themeKey='container'
+      >
+        {children}
+      </Box>
+    </Box>
+  )
+})
+
+Header.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+    .isRequired,
+  shadow: PropTypes.bool,
+}
+
+Header.defaultProps = {
+  shadow: false,
+}
+
+export default Header
---
+++ packages/gatsby-theme/src/components/Header/HeaderBranding.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types"
+import { Box } from "theme-ui"
+import Link from "../Link"
+
+const HeaderBranding = ({ children, to, ariaLabel }) => {
+  return (
+    <Box
+      __css={{
+        display: "flex",
+        mx: ["auto", 0],
+        mt: "0",
+        flexWrap: "wrap",
+        height: ["auto", null, 80],
+        pl: ["small", null, "medium"],
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to={to} ariaLabel={ariaLabel}>
+        {children}
+      </Link>
+    </Box>
+  )
+}
+
+HeaderBranding.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+    .isRequired,
+  to: PropTypes.string.isRequired,
+  ariaLabel: PropTypes.string.isRequired,
+}
+
+export default HeaderBranding
