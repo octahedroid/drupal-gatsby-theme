7620db95a83e0933b9eaa27b6dd013fe96a76fec
Carlos Dinarte <carlos@octahedroid.com>
2020-12-04 11:24:07 -0600

    Rollback: add React import to components

--- packages/gatsby-theme/src/components/BlogHead/BlogHeadTitle.js
+++ packages/gatsby-theme/src/components/BlogHead/BlogHeadTitle.js
@@ -1,3 +1,4 @@
+import React from "react"
 import PropTypes from "prop-types"
 import Heading from "../Heading"
 
--- packages/gatsby-theme/src/components/HeroCta/HeroCta.js
+++ packages/gatsby-theme/src/components/HeroCta/HeroCta.js
@@ -1,4 +1,4 @@
-import { forwardRef } from "react"
+import React, { forwardRef } from "react"
 import PropTypes from "prop-types"
 import { Box } from "theme-ui"
 import Grid from "../Grid"
