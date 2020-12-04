94d0ee4bdc62cf3afa660cc039ad3c0f1bb0b15f
edvicaty <edvicaty@hotmail.com>
2020-12-04 12:05:27 -0600

    refactor: remove unnecesary footer files and imports

--- packages/gatsby-theme/src/components/Footer/index.js
+++ packages/gatsby-theme/src/components/Footer/index.js
@@ -1,24 +1,6 @@
 import Footer from "./Footer"
 import FooterBranding from "./FooterBranding"
-// import FooterIcon from "./FooterIcon"
-// import FooterLink from "./FooterLink"
-// import FooterLinkGroup from "./FooterLinkGroup"
-// import FooterList from "./FooterList"
-// import FooterMenuColumn from "./FooterMenuColumn"
-// import FooterMenuItem from "./FooterMenuItem"
-// import FooterMenuTitle from "./FooterMenuTitle"
-// import FooterNavigation from "./FooterNavigation"
-// import FooterNavItem from "./FooterNavItem"
 
 Footer.Branding = FooterBranding
-// Footer.Icon = FooterIcon
-// Footer.Link = FooterLink
-// Footer.LinkGroup = FooterLinkGroup
-// Footer.List = FooterList
-// Footer.MenuColumn = FooterMenuColumn
-// Footer.MenuItem = FooterMenuItem
-// Footer.MenuTitle = FooterMenuTitle
-// Footer.Navigation = FooterNavigation
-// Footer.NavItem = FooterNavItem
 
 export default Footer
