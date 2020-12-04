a7c154efd56ee82711208a6b69a37e4f387466c9
Carlos Dinarte <carlos@octahedroid.com>
2020-12-04 11:20:55 -0600

    Rollback: add React import to blocks

--- packages/gatsby-theme/src/blocks/HeroCtaBlock/index.js
+++ packages/gatsby-theme/src/blocks/HeroCtaBlock/index.js
@@ -1,3 +1,4 @@
+import React from "react"
 import PropTypes from "prop-types"
 import HeroCta from "../../components/HeroCta"
 
--- packages/gatsby-theme/src/blocks/HeroTextBlock/index.js
+++ packages/gatsby-theme/src/blocks/HeroTextBlock/index.js
@@ -1,3 +1,4 @@
+import React from "react"
 import PropTypes from "prop-types"
 import HeroText from "../../components/HeroText"
 import Section from "../../components/Section"
