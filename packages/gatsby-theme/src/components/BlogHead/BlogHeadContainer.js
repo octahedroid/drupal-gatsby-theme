79af4b614053a5bc5cd6c6048da1e520f1aa32a5
RosaIsela31 <rosa@octahedroid.com>
2021-02-12 14:42:06 -0600

    Simplify the BlogHead component

--- packages/gatsby-theme/src/components/BlogHead/BlogHeadContainer.js
+++ packages/gatsby-theme/src/components/BlogHead/BlogHeadContainer.js
@@ -1,8 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Box } from "theme-ui"
+import { Heading, Link, Icon } from "ui"
 
-const BlogHeadContainer = ({ children, ...props }) => {
+const BlogHeadContainer = ({ author, ariaLabel, date, title, to, sx, ...props }) => {
   return (
     <Box
       as='section'
@@ -17,16 +18,53 @@ const BlogHeadContainer = ({ children, ...props }) => {
         flexDirection: "row",
         flexWrap: "wrap",
         justifyContent: "space-between",
+        ...sx,
       }}
       {...props}
     >
-      {children}
+      <Heading
+        as='h1'
+        sx={{ fontSize: ["4xl", "5xl"], mb: "small", minWidth: "100%", ...sx }}
+      >
+        {title}
+      </Heading>
+      <Box __css={{ pr: "medium" }}>
+        {to ? (
+          <Link
+            to={to}
+            ariaLabel={ariaLabel}
+            sx={{ color: "inherit", cursor: "pointer" }}
+          >
+            <Icon icon='user' sx={{ mr: "xsmall", pt: 2 }} />
+            {author}
+          </Link>
+        ) : (
+          <>
+            <Icon icon='user' sx={{ mr: "xsmall", pt: 2 }} />
+            {author}
+          </>
+        )}
+      </Box>
+      <Box __css={{ pr: "medium" }}>
+        <Icon icon='calendar' sx={{ mr: "xsmall", pt: 2 }} />
+        {date}
+      </Box>
     </Box>
   )
 }
 
 BlogHeadContainer.propTypes = {
-  children: PropTypes.node.isRequired,
+  author: PropTypes.string.isRequired,
+  ariaLabel: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  to: PropTypes.string,
+  sx: PropTypes.shape({}),
+}
+
+BlogHeadContainer.defaultProps = {
+  to: undefined,
+  sx: undefined,
 }
 
 export default BlogHeadContainer
