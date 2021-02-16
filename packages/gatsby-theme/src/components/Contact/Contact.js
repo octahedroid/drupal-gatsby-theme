badbed0f0571f37d65a49817037487430895abd0
Rosa Isela López Palma <50505959+RosaIsela31@users.noreply.github.com>
2021-02-16 15:59:27 -0600

    Refactor: Contact component and ContactBlock (#40)
    * Refactor: Contact component and ContactBlock
    
    * resolving suggestions

--- packages/gatsby-theme/src/components/Contact/Contact.js
+++ packages/gatsby-theme/src/components/Contact/Contact.js
@@ -1,10 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Box } from "theme-ui"
+import { RichText } from "ui"
 
-const Contact = ({ children, ...props }) => {
+const Contact = ({ children, title, align, sx, ...props }) => {
   return (
-    <Box variant='wrapper' __themeKey='container' {...props}>
+    <Box
+      variant='wrapper'
+      __themeKey='container'
+      __css={{ textAlign: align, ...sx }}
+      {...props}
+    >
+      <RichText sx={{ textAlign: align, pb: "small" }} {...props}>
+        {title}
+      </RichText>
       {children}
     </Box>
   )
@@ -12,6 +21,14 @@ const Contact = ({ children, ...props }) => {
 
 Contact.propTypes = {
   children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+  sx: PropTypes.shape({}),
+  align: PropTypes.string,
+}
+
+Contact.defaultProps = {
+  align: "center",
+  sx: undefined,
 }
 
 export default Contact
