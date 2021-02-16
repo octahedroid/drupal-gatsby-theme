1f1ee5731bd395c0806c1f3fd74a5d121eca02fb
Rosa Isela López Palma <50505959+RosaIsela31@users.noreply.github.com>
2021-02-16 15:59:10 -0600

    Refactor: Share component (#39)
    * Refactor: Share component and block
    
    * fix stories
    
    * resolve suggestions
    
    * resolving a suggestion

--- packages/gatsby-theme/src/blocks/ShareBlock/index.js
+++ packages/gatsby-theme/src/blocks/ShareBlock/index.js
@@ -3,33 +3,30 @@ import PropTypes from "prop-types"
 import { Box } from "theme-ui"
 import { Share } from "ui"
 
-const ShareBlock = ({ heading, data, ...props }) => {
+const ShareBlock = ({ data, sx, ...props }) => {
   return (
-    <Box variant='wrapper' __themeKey='container'>
+    <Box variant='wrapper' __themeKey='container' sx={sx}>
       <Share {...props}>
-        {heading && <Share.Heading>{heading}</Share.Heading>}
-        <Share.Container>
-          {data.map((item) => {
-            return !item.disabled && <Share.Social {...item} key={item.social} />
-          })}
-        </Share.Container>
+        {data.map((item) => {
+          return !item.disabled && <Share.Social {...item} key={item.social} />
+        })}
       </Share>
     </Box>
   )
 }
 
 ShareBlock.propTypes = {
-  heading: PropTypes.string,
   data: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string,
       to: PropTypes.string,
     })
   ).isRequired,
+  sx: PropTypes.shape({}),
 }
 
 ShareBlock.defaultProps = {
-  heading: "Share: ",
+  sx: undefined,
 }
 
 export default ShareBlock
