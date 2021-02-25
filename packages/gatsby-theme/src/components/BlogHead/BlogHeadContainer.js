c95983236251b5e8a631e2dd7bf84264089076ae
Rosa Isela López Palma <50505959+RosaIsela31@users.noreply.github.com>
2021-02-25 16:18:48 -0600

    Simplify the BlogHead component (#44)
    * Simplify the BlogHead component
    
    * refactor finished for the BlogHead component
    
    * add the BlogHeadBlock implementation
    
    * removing unnecessary OR operator
    
    * create autho object with the author name and arialabel
    
    * create a const with the icon styles
    
    * change name from authorLabel to label
    
    * change authorlabel to label into the story

--- packages/gatsby-theme/src/components/BlogHead/BlogHeadContainer.js
+++ packages/gatsby-theme/src/components/BlogHead/BlogHeadContainer.js
@@ -1,8 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Box } from "theme-ui"
+import { Heading, Link, Icon } from "ui"
 
-const BlogHeadContainer = ({ children, ...props }) => {
+const BlogHeadContainer = ({ credits, author, date, title, to, sx, ...props }) => {
+  const iconStyles = {
+    mr: "xsmall",
+    pt: 2,
+  }
+
   return (
     <Box
       as='section'
@@ -17,16 +23,73 @@ const BlogHeadContainer = ({ children, ...props }) => {
         flexDirection: "row",
         flexWrap: "wrap",
         justifyContent: "space-between",
+        ...sx,
       }}
       {...props}
     >
-      {children}
+      <Heading
+        as='h1'
+        sx={{ fontSize: ["4xl", "5xl"], mb: "small", minWidth: "100%", ...sx }}
+      >
+        {title}
+      </Heading>
+      <Box __css={{ pr: "medium" }}>
+        {to ? (
+          <Link
+            to={to}
+            ariaLabel={author.label || author.name}
+            sx={{ color: "inherit", cursor: "pointer" }}
+          >
+            <Icon icon='user' sx={{ ...iconStyles }} />
+            {author.name}
+          </Link>
+        ) : (
+          <>
+            <Icon icon='user' sx={{ ...iconStyles }} />
+            {author.name}
+          </>
+        )}
+      </Box>
+      <Box __css={{ pr: "medium" }}>
+        <Icon icon='calendar' sx={{ ...iconStyles }} />
+        {date}
+      </Box>
+      {credits && (
+        <Box
+          __css={{
+            position: "absolute",
+            zIndex: 4,
+            bottom: "small",
+            left: "medium",
+            fontWeight: "thin",
+            fontSize: "tiny",
+            fontStyle: "italic",
+          }}
+          {...props}
+        >
+          {credits}
+        </Box>
+      )}
     </Box>
   )
 }
 
 BlogHeadContainer.propTypes = {
-  children: PropTypes.node.isRequired,
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string,
+  }).isRequired,
+  credits: PropTypes.string,
+  date: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  to: PropTypes.string,
+  sx: PropTypes.shape({}),
+}
+
+BlogHeadContainer.defaultProps = {
+  credits: undefined,
+  to: undefined,
+  sx: undefined,
 }
 
 export default BlogHeadContainer
